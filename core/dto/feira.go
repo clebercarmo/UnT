c0c01d600a0fcbd4eb8f13f7c5339476b3e55715
Cleber Carmo <rcleber@outlook.com.br>
2022-08-23 11:57:54 -0300

    feat: news methods feira.dto

--- core/dto/feira.go
+++ core/dto/feira.go
@@ -6,27 +6,48 @@ import (
 )
 
 // CreateFeiraRequest uma representação do request para criação de um novo registro
-type CreateFeiraRequest struct{
-	Id         int16	`json:"id"`
-	Long       string   `json:"long"`
-	Lat        string   `json:"lat"`
-	SetCens    string   `json:"setcens"`
-	AreaP      string	`json:"areap"`
-	CodDist    string	`json:"coddist"`
-	Distrito   string	`json:"distrito"`
-	CodSubPref string	`json:"codsubpref"`
-	SubPrere   string	`json:"subprere"`
-	Regiao5    string	`json:"regiao5"`
-	Regiao8    string	`json:"regiao8"`
-	NomeFreira string	`json:"nomefreira"`
-	Registo    string	`json:"registro"`
-	Logradouro string	`json:"logradouro"`
-	Numero     string	`json:"numero"`
-	Bairro     string	`json:"bairro"`
-	Referencia string	`json:"referencia"`
-
+type CreateFeiraRequest struct {
+	Id         int16  `json:"id"`
+	Long       string `json:"long"`
+	Lat        string `json:"lat"`
+	SetCens    string `json:"setcens"`
+	AreaP      string `json:"areap"`
+	CodDist    string `json:"coddist"`
+	Distrito   string `json:"distrito"`
+	CodSubPref string `json:"codsubpref"`
+	SubPrere   string `json:"subprere"`
+	Regiao5    string `json:"regiao5"`
+	Regiao8    string `json:"regiao8"`
+	NomeFreira string `json:"nomefreira"`
+	Registo    string `json:"registro"`
+	Logradouro string `json:"logradouro"`
+	Numero     string `json:"numero"`
+	Bairro     string `json:"bairro"`
+	Referencia string `json:"referencia"`
 }
 
+type UpdateFeiraRequest struct {
+	Long       string `json:"long"`
+	Lat        string `json:"lat"`
+	SetCens    string `json:"setcens"`
+	AreaP      string `json:"areap"`
+	CodDist    string `json:"coddist"`
+	Distrito   string `json:"distrito"`
+	CodSubPref string `json:"codsubpref"`
+	SubPrere   string `json:"subprere"`
+	Regiao5    string `json:"regiao5"`
+	Regiao8    string `json:"regiao8"`
+	NomeFreira string `json:"nomefreira"`
+	Registo    string `json:"registro"`
+	Logradouro string `json:"logradouro"`
+	Numero     string `json:"numero"`
+	Bairro     string `json:"bairro"`
+	Referencia string `json:"referencia"`
+}
+
+type DeleteFeiraRequest struct {
+	Id int16 `json:"id"`
+}
 
 // FromJSONCreateFeiraRequest converte json para um CreateFeiraRequest struct
 func FromJSONCreateFeiraRequest(body io.Reader) (*CreateFeiraRequest, error) {
@@ -36,4 +57,25 @@ func FromJSONCreateFeiraRequest(body io.Reader) (*CreateFeiraRequest, error) {
 	}
 
 	return &CreateFeiraRequest, nil
+}
+
+// FromJSONUpdateFeiraRequest converte json para um UpdateFeiraRequest struct
+func FromJSONUpdateFeiraRequest(body io.Reader) (*UpdateFeiraRequest, error) {
+	UpdateFeiraRequest := UpdateFeiraRequest{}
+	if err := json.NewDecoder(body).Decode(&UpdateFeiraRequest); err != nil {
+		return nil, err
+	}
+
+	return &UpdateFeiraRequest, nil
+}
+
+
+// FromJSONUpdateFeiraRequest converte json para um DeleteFeiraRequest struct
+func FromJSONDeleteFeiraRequest(body io.Reader) (*DeleteFeiraRequest, error) {
+	DeleteFeiraRequest := DeleteFeiraRequest{}
+	if err := json.NewDecoder(body).Decode(&DeleteFeiraRequest); err != nil {
+		return nil, err
+	}
+
+	return &DeleteFeiraRequest, nil
 }
--- core/dto/feira_test.go
+++ core/dto/feira_test.go
@@ -45,3 +45,64 @@ func TestFromJSONCreateFeiraRequest_JSONDecodeError(t *testing.T) {
 	require.NotNil(t, err)
 	require.Nil(t, itemRequest)
 }
+
+
+func TestFromJSONUpdateFeiraRequest(t *testing.T) {
+
+	fakeItem := dto.UpdateFeiraRequest{}
+	faker.FakeData(&fakeItem)
+
+	json, err := json.Marshal(fakeItem)
+	require.Nil(t, err)
+
+	itemRequest, err := dto.FromJSONUpdateFeiraRequest(strings.NewReader(string(json)))
+
+	require.Nil(t, err)	
+	require.Equal(t, itemRequest.Lat, fakeItem.Lat)
+	require.Equal(t, itemRequest.Long, fakeItem.Long)
+	require.Equal(t, itemRequest.SetCens, fakeItem.SetCens)
+	require.Equal(t, itemRequest.AreaP, fakeItem.AreaP)
+	require.Equal(t, itemRequest.CodDist, fakeItem.CodDist)
+	require.Equal(t, itemRequest.Distrito, fakeItem.Distrito)
+	require.Equal(t, itemRequest.CodSubPref, fakeItem.CodSubPref)
+	require.Equal(t, itemRequest.SubPrere, fakeItem.SubPrere)
+	require.Equal(t, itemRequest.Regiao5, fakeItem.Regiao5)
+	require.Equal(t, itemRequest.Regiao8, fakeItem.Regiao8)
+	require.Equal(t, itemRequest.NomeFreira, fakeItem.NomeFreira)
+	require.Equal(t, itemRequest.Registo, fakeItem.Registo)
+	require.Equal(t, itemRequest.Logradouro, fakeItem.Logradouro)
+	require.Equal(t, itemRequest.Numero, fakeItem.Numero)
+	require.Equal(t, itemRequest.Bairro, fakeItem.Bairro)
+	require.Equal(t, itemRequest.Referencia, fakeItem.Referencia)
+
+}
+
+
+func TestFromJSONUpdateFeiraRequest_JSONDecodeError(t *testing.T) {
+	itemRequest, err := dto.FromJSONUpdateFeiraRequest(strings.NewReader("{"))
+
+	require.NotNil(t, err)
+	require.Nil(t, itemRequest)
+}
+
+
+func TestFromJSONDeleteFeiraRequest(t *testing.T) {
+
+	fakeItem := dto.DeleteFeiraRequest{}
+	faker.FakeData(&fakeItem)
+
+	json, err := json.Marshal(fakeItem)
+	require.Nil(t, err)
+
+	itemRequest, err := dto.FromJSONDeleteFeiraRequest(strings.NewReader(string(json)))
+	require.Nil(t, err)	
+	require.Equal(t, itemRequest.Id, fakeItem.Id)
+
+}
+
+func TestFromJSONDeleteFeiraRequest_JSONDecodeError(t *testing.T) {
+	itemRequest, err := dto.FromJSONDeleteFeiraRequest(strings.NewReader("{"))
+
+	require.NotNil(t, err)
+	require.Nil(t, itemRequest)
+}
